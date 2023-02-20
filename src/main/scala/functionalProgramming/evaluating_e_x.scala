24155423880cf2bb84396ed0f18f8a59bbcd9ae6
Tio da Castanha <almosttherealan@gmail.com>
2023-02-20 08:57:18 -0300

    add

--- src/main/scala/functionalProgramming/evaluating_e_x.scala
+++ src/main/scala/functionalProgramming/evaluating_e_x.scala
@@ -1,14 +1,17 @@
 package functionalProgramming
 
+import scala.annotation.tailrec
+
 object evaluating_e_x extends App {
 
   val stdin = scala.io.StdIn
+
   val n = stdin.readLine.trim.toInt
+  def fatorial (x: Int): Int = (1 to x).product
 
-  for (i <- 1 to n) {
+  for (nItr <- 1 to n) {
     val x = stdin.readLine.trim.toDouble
-
-
+    printf("%.4f\n", (2 to 9).map(a => math.pow(x, a) / fatorial(a)).toList.sum + 1 + x)
   }
 
 }
